7baa848a23ad8fac8e0bfe6133601a80635e37ee
Luong The Tai <tailtph35654@fpt.edu.vn>
2023-05-11 15:53:18 +0700

    Initial commit

---
+++ app/src/main/java/com/example/lap1ph35654_luongthetai/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.lap1ph35654_luongthetai;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.bai3);
+            }
+        });
+
+
+
+
+    }
+}
